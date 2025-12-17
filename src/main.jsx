91ea556c6818ac1ce0e438d3faf649fdb5c7f15f
Stewin Navin Mathias <mathiasstewinn@gmail.com>
2025-12-17 17:05:07 +0530

    Force deployment update to apply 3D effects

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Force deployment update
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
