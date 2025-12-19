60503cca4a595886915e0826f5322857dea5ff17
Stewin Navin Mathias <mathiasstewinn@gmail.com>
2025-12-20 01:58:43 +0530

    Final polish: Mobile layout fixes and responsive adjustments

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-// Force deployment update - Hide festival doodle on mobile
+// Force deployment update - Final polish
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
